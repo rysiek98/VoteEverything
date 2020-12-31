ac7d3ce348e6e44fd49152d1164884783235fdd9
rysiek98 <52510889+rysiek98@users.noreply.github.com>
2020-12-31 12:45:43 +0100

    Fixed votes display

--- app/src/main/java/com/example/voteeverything/ViewVotesActivity.kt
+++ app/src/main/java/com/example/voteeverything/ViewVotesActivity.kt
@@ -1,21 +1,27 @@
 package com.example.voteeverything
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_view_votes.*
+
 
 class ViewVotesActivity : AppCompatActivity() {
 
@@ -41,10 +47,12 @@ class ViewVotesActivity : AppCompatActivity() {
         val title = intent.getStringExtra("title")
         val userUID = intent.getStringExtra("userUID")
         val flag = intent.getBooleanExtra("flag", false)
-        val docRefSurvey = db.collection(userUID).document("S_"+title)
+        val docRefSurvey = db.collection(userUID).document("S_" + title)
         val docRefUserSurvey = db.collection("dbInfo").document("surveys")
         var options: ArrayList<String>
+        var xAxisLabels: ArrayList<String> = ArrayList()
         val votes: ArrayList<Float> = ArrayList()
+        val container = findViewById<View>(R.id.viewVotesContainer) as LinearLayout
 
         if(flag){
             deleteVVotesBt.visibility = View.VISIBLE
@@ -58,14 +66,18 @@ class ViewVotesActivity : AppCompatActivity() {
                 if(DocumentSnapshot.exists()){
                     votes.addAll(DocumentSnapshot.get("votes") as ArrayList<Float>)
                     options = DocumentSnapshot.get("options") as ArrayList<String>
-                    setBarChart(votes, options)
+                    for (i in 0 until options.size){
+                        xAxisLabels.add("Opcja ${i+1}")
+                    }
+                    addOptions(xAxisLabels, options, container,this)
+                    setBarChart(votes, xAxisLabels)
                 }else{
                     Toast.makeText(baseContext, "Data not found!", Toast.LENGTH_SHORT)
                         .show()
                 }
             }
             .addOnFailureListener {
-                Toast.makeText(baseContext,"Data not found!", Toast.LENGTH_SHORT)
+                Toast.makeText(baseContext, "Data not found!", Toast.LENGTH_SHORT)
                     .show()
             }
         titleVVotes.text = title
@@ -73,6 +85,18 @@ class ViewVotesActivity : AppCompatActivity() {
         backVVotesBt.setOnClickListener {
             finish()
         }
+    }
+
+    private fun addOptions(xAxisLabel: ArrayList<String>, options: ArrayList<String>,container: LinearLayout, context: Context) {
+        var i = 0
+        options.forEach { option ->
+           val newElement = TextView(context)
+           newElement.text = xAxisLabel[i]+": " + option
+           newElement.textSize = 18f
+           newElement.setTextColor(Color.BLACK)
+           container.addView(newElement)
+           i++
+       }
     }
 
 
@@ -90,8 +114,8 @@ class ViewVotesActivity : AppCompatActivity() {
                     userToSurvey = DocumentSnapshot.get("userToSurvey") as ArrayList<String>
                     for (i in 0 until userToSurvey.size){
                         if (userToSurvey[i] == userUID){
-                            if (userToSurvey[i+1] == title){
-                                userToSurvey[i+1] = "0"
+                            if (userToSurvey[i + 1] == title){
+                                userToSurvey[i + 1] = "0"
                                 userToSurvey[i] = "0"
                             }
                         }
@@ -99,16 +123,16 @@ class ViewVotesActivity : AppCompatActivity() {
                     userToSurvey.removeIf { field -> field == "0" }
                     updateSurveys(userToSurvey, db)
                 }else{
-                    Toast.makeText(baseContext,"Data not found!",Toast.LENGTH_SHORT)
+                    Toast.makeText(baseContext, "Data not found!", Toast.LENGTH_SHORT)
                         .show()
                 }
             }
             .addOnFailureListener {
-                Toast.makeText(baseContext,"Data not found!", Toast.LENGTH_SHORT)
+                Toast.makeText(baseContext, "Data not found!", Toast.LENGTH_SHORT)
                     .show()
             }
         docRefSurvey.delete()
-        Toast.makeText(baseContext,"Data deleted from database", Toast.LENGTH_SHORT)
+        Toast.makeText(baseContext, "Data deleted from database", Toast.LENGTH_SHORT)
             .show()
         Handler().postDelayed({ finish() }, 500)
     }
@@ -116,25 +140,20 @@ class ViewVotesActivity : AppCompatActivity() {
     private fun setBarChart(votes: ArrayList<Float>, xAxisLabels: ArrayList<String>) {
         val entries = ArrayList<BarEntry>()
         val colors = ArrayList<Int>()
-        var r = 50
-        var g = 100
-        var b = 80
+        var r = 255
+        var g = 0
+        var b = 0
         var it = 0f
         var votesSum = 0f
         votes.forEach { vote -> votesSum += vote }
         votes.forEach { vote ->
-            entries.add(BarEntry(it, (vote/votesSum)*100))
-            if (r < 255 && g < 240 && b < 245 ) {
-                r += 5
-                g += 25
-                b += 10
-            }else{
-                r = 50
-                g = 100
-                b = 80
-            }
-            colors.add(Color.rgb(r,g,b))
+            entries.add(BarEntry(it, (vote / votesSum) * 100))
+            colors.add(Color.rgb(r, g, b))
             it++
+            r = (20..254).random()
+            g = (20..254).random()
+            b = (20..254).random()
+
         }
         val barDataSet = BarDataSet(entries, "Total number of votes: $votesSum")
         barDataSet.valueTextSize = 14f
@@ -149,7 +168,12 @@ class ViewVotesActivity : AppCompatActivity() {
         barChart.description.isEnabled = false
         barChart.setVisibleXRangeMaximum(5f)
         barChart.xAxis.labelCount = votes.size
-        barChart.moveViewToX((votes.size/2).toFloat())
+        barChart.xAxis.xOffset = 15f
+        barChart.xAxis.granularity = 1f
+        barChart.xAxis.setDrawLabels(true)
+        barChart.xAxis.spaceMin = 5f
+        barChart.xAxis.spaceMax = 25f
+        barChart.moveViewToX((votes.size / 2).toFloat())
         barChart.setPinchZoom(true)
         barChart.invalidate()
         barChart.animateY(2500)
@@ -160,7 +184,7 @@ class ViewVotesActivity : AppCompatActivity() {
         if (hasFocus) hideSystemUI(window)
     }
 
-    private fun updateSurveys( surveys: ArrayList<String>, db: FirebaseFirestore){
+    private fun updateSurveys(surveys: ArrayList<String>, db: FirebaseFirestore){
         val data   = hashMapOf(
             "userToSurvey" to surveys
         )
